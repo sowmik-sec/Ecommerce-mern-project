bc7a1cbf8accd679a99970fccc8bc9e5f519aecb
sowmik-sec <sowmik.sec@gmail.com>
2024-05-15 12:56:54 +0600

    set start script to package.json file. set morgan package dependency as dev dependency

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/test", (req, res) => {
   res.status(200).send({ message: "api is working fine" });
