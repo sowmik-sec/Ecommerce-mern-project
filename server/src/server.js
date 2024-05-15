80e99d6252b2080de38cece28d1453c2bee9d87d
sowmik-sec <sowmik.sec@gmail.com>
2024-05-15 13:15:04 +0600

    middlewares in express

--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,25 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const isLoggedIn = (req, res, next) => {
+  const login = true;
+  if (login) {
+    req.body.id = 101;
+    next();
+  } else {
+    return res.status(401).json({ message: "Please login first" });
+  }
+};
 
 app.get("/test", (req, res) => {
   res.status(200).send({ message: "api is working fine" });
+});
+app.get("/api/user", isLoggedIn, (req, res) => {
+  console.log(req.body.id);
+  res.status(200).send({ message: "user profile is returned" });
 });
 
 app.listen(3001, () => {
