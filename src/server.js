d812b29c34deaf72472b7b0178adfcdcc2bcc637
sowmik-sec <sowmik.sec@gmail.com>
2024-05-15 12:41:09 +0600

    http request: get

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.status(200).send({ message: "api is working fine" });
+});
+
 app.listen(3001, () => {
   console.log(`server is running at http://localhost:3001`);
 });
